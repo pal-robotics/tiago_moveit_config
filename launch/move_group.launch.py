345e18187ce3e338bc0c2a1f0551547c0d4cf454
Victor Lopez <victor.lopez@pal-robotics.com>
2021-07-20 15:33:38 +0200

    Update move_group for all tiago configurations

--- launch/move_group.launch.py
+++ launch/move_group.launch.py
@@ -2,31 +2,21 @@ import os
 import yaml
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
+from launch_pal.substitutions import LoadFile
 from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
 import xacro
 
 from tiago_description.tiago_launch_utils import (get_tiago_hw_arguments,
+                                                  get_tiago_hw_suffix,
                                                   generate_robot_description_action)
 
-
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, "r") as file:
-            return file.read()
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
-
-
 def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
+    package_path=get_package_share_directory(package_name)
+    absolute_file_path=os.path.join(package_path, file_path)
 
     try:
         with open(absolute_file_path, "r") as file:
@@ -37,70 +27,73 @@ def load_yaml(package_name, file_path):
 
 def generate_launch_description():
     # TIAGo description arguments
-    tiago_args = get_tiago_hw_arguments(
+    tiago_args=get_tiago_hw_arguments(
         laser_model=True,
         arm=True,
         end_effector=True,
         ft_sensor=True)
 
     # Command-line arguments
-    tutorial_arg = DeclareLaunchArgument(
-        "rviz_tutorial", default_value="False", description="Tutorial flag"
+    sim_time_arg=DeclareLaunchArgument(
+        "use_sim_time", default_value="True", description="Use sim time"
     )
 
     # Read robot descripton from tiago_description
-    robot_description = {"robot_description": generate_robot_description_action()}
+    robot_description={
+        "robot_description": generate_robot_description_action()}
 
-    # TODO Hard coded for now for testing
-    robot_description_semantic_config = load_file(
-        "tiago_moveit_config", "config/srdf/tiago_pal-gripper.srdf"
+    robot_description_semantic_config=LoadFile(
+        [get_package_share_directory("tiago_moveit_config"), "/config/srdf/tiago_",
+        get_tiago_hw_suffix(arm=True, wrist_model=False,
+                            end_effector=True, ft_sensor=True), ".srdf"]
     )
-    robot_description_semantic = {
+    robot_description_semantic={
         "robot_description_semantic": robot_description_semantic_config
     }
 
-    kinematics_yaml = load_yaml(
+    kinematics_yaml=load_yaml(
         "tiago_moveit_config", "config/kinematics_kdl.yaml"
     )
 
     # Planning Functionality
-    ompl_planning_pipeline_config = {
+    ompl_planning_pipeline_config={
         "move_group": {
             "planning_plugin": "ompl_interface/OMPLPlanner",
             "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/ResolveConstraintFrames default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
             "start_state_max_bounds_error": 0.1,
         }
     }
-    ompl_planning_yaml = load_yaml(
+    ompl_planning_yaml=load_yaml(
         "tiago_moveit_config", "config/ompl_planning.yaml"
     )
     ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
 
     # Trajectory Execution Functionality
-    moveit_simple_controllers_yaml = load_yaml(
-        "tiago_moveit_config", "config/controllers/controllers_pal-gripper.yaml"
-    )
-    moveit_controllers = {
-        "moveit_simple_controller_manager": moveit_simple_controllers_yaml,
-        "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
-    }
+    moveit_simple_controllers_path = [
+        get_package_share_directory('tiago_moveit_config'), "/config/controllers/controllers_",
+        get_tiago_hw_suffix(arm=True, wrist_model=False,
+                            end_effector=True, ft_sensor=True), ".yaml"]
 
-    trajectory_execution = {
+    trajectory_execution={
         "moveit_manage_controllers": True,
         "trajectory_execution.allowed_execution_duration_scaling": 1.2,
         "trajectory_execution.allowed_goal_duration_margin": 0.5,
         "trajectory_execution.allowed_start_tolerance": 0.01,
     }
 
-    planning_scene_monitor_parameters = {
+    planning_scene_monitor_parameters={
         "publish_planning_scene": True,
         "publish_geometry_updates": True,
         "publish_state_updates": True,
         "publish_transforms_updates": True,
     }
 
+    sim_time_parameters={
+        "use_sim_time": LaunchConfiguration("use_sim_time")
+    }
+
     # Start the actual move_group node/action server
-    run_move_group_node = Node(
+    run_move_group_node=Node(
         package="moveit_ros_move_group",
         executable="move_group",
         output="screen",
@@ -110,32 +103,19 @@ def generate_launch_description():
             kinematics_yaml,
             ompl_planning_pipeline_config,
             trajectory_execution,
-            moveit_controllers,
+            moveit_simple_controllers_path,
             planning_scene_monitor_parameters,
+            sim_time_parameters,
         ],
     )
 
     # RViz
-    tutorial_mode = LaunchConfiguration("rviz_tutorial")
-    rviz_base = os.path.join(get_package_share_directory(
+    tutorial_mode=LaunchConfiguration("rviz_tutorial")
+    rviz_base=os.path.join(get_package_share_directory(
         "moveit_resources_panda_moveit_config"), "launch")
-    rviz_full_config = os.path.join(rviz_base, "moveit.rviz")
-    rviz_empty_config = os.path.join(rviz_base, "moveit_empty.rviz")
-    rviz_node_tutorial = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_empty_config],
-        parameters=[
-            robot_description,
-            robot_description_semantic,
-            ompl_planning_pipeline_config,
-            kinematics_yaml,
-        ],
-        condition=IfCondition(tutorial_mode),
-    )
-    rviz_node = Node(
+    rviz_full_config=os.path.join(rviz_base, "moveit.rviz")
+    rviz_empty_config=os.path.join(rviz_base, "moveit_empty.rviz")
+    rviz_node=Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
@@ -147,15 +127,13 @@ def generate_launch_description():
             ompl_planning_pipeline_config,
             kinematics_yaml,
         ],
-        condition=UnlessCondition(tutorial_mode),
     )
 
     return LaunchDescription(
         [
             *tiago_args,
-            tutorial_arg,
+            sim_time_arg,
             rviz_node,
-            rviz_node_tutorial,
             run_move_group_node,
         ]
     )
