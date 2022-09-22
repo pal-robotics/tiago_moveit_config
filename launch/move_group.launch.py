a17e7d52369a0ee62979ac602ed41d0b0a5a22e2
Noel Jimenez <noel.jimenez@pal-robotics.com>
2022-09-22 12:44:37 +0200

    update moveit launchers refactor

--- launch/move_group.launch.py
+++ launch/move_group.launch.py
@@ -1,45 +1,52 @@
 import os
-import yaml
+from pathlib import Path
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch_pal.arg_utils import read_launch_argument
+from launch_pal.robot_utils import (get_arm,
+                                    get_camera_model,
+                                    get_end_effector,
+                                    get_ft_sensor,
+                                    get_laser_model,
+                                    get_robot_name,
+                                    get_wrist_model)
 from launch_pal.substitutions import LoadFile
 from ament_index_python.packages import get_package_share_directory
 
-from tiago_description.tiago_launch_utils import (get_tiago_hw_arguments,
-                                                  get_tiago_hw_suffix,
-                                                  generate_robot_description_action)
+from tiago_description.tiago_launch_utils import get_tiago_hw_suffix
+from launch_param_builder import load_xacro, load_yaml
 
 
-def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
+def declare_args(context, *args, **kwargs):
 
-    try:
-        with open(absolute_file_path, "r") as file:
-            return yaml.safe_load(file)
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
+    robot_name = read_launch_argument('robot_name', context)
 
-
-def generate_launch_description():
     # TIAGo description arguments
-    tiago_args = get_tiago_hw_arguments(
-        laser_model=True,
-        arm=True,
-        end_effector=True,
-        ft_sensor=True,
-        camera_model=True)
+    return [get_arm(robot_name),
+            get_camera_model(robot_name),
+            get_end_effector(robot_name),
+            get_ft_sensor(robot_name),
+            get_laser_model(robot_name),
+            get_wrist_model(robot_name)]
 
-    # Command-line arguments
-    sim_time_arg = DeclareLaunchArgument(
-        "use_sim_time", default_value="True", description="Use sim time"
-    )
 
-    # Read robot descripton from tiago_description
-    robot_description = {
-        "robot_description": generate_robot_description_action()}
+def launch_setup(context, *args, **kwargs):
+
+    robot_description = {'robot_description': load_xacro(
+        Path(os.path.join(
+            get_package_share_directory('tiago_description'), 'robots', 'tiago.urdf.xacro')),
+        {
+            'arm': read_launch_argument('arm', context),
+            'camera_model': read_launch_argument('camera_model', context),
+            'end_effector': read_launch_argument('end_effector', context),
+            'ft_sensor': read_launch_argument('ft_sensor', context),
+            'laser_model': read_launch_argument('laser_model', context),
+            'wrist_model': read_launch_argument('wrist_model', context),
+        },
+    )}
 
     robot_description_semantic_config = LoadFile(
         [get_package_share_directory("tiago_moveit_config"), "/config/srdf/tiago_",
@@ -51,7 +58,8 @@ def generate_launch_description():
     }
 
     kinematics_yaml = load_yaml(
-        "tiago_moveit_config", "config/kinematics_kdl.yaml"
+        Path(os.path.join(
+            get_package_share_directory('tiago_moveit_config'), 'config', 'kinematics_kdl.yaml'))
     )
 
     # Planning Functionality
@@ -63,7 +71,8 @@ def generate_launch_description():
         }
     }
     ompl_planning_yaml = load_yaml(
-        "tiago_moveit_config", "config/ompl_planning.yaml"
+        Path(os.path.join(
+            get_package_share_directory('tiago_moveit_config'), 'config', 'ompl_planning.yaml'))
     )
     ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
 
@@ -109,10 +118,24 @@ def generate_launch_description():
         ],
     )
 
+    return [run_move_group_node]
+
+
+def generate_launch_description():
+    # Command-line arguments
+    sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time", default_value="True", description="Use sim time"
+    )
+
     ld = LaunchDescription()
 
-    ld.add_action(*tiago_args)
+    # Declare arguments
+    # we use OpaqueFunction so the callbacks have access to the context
+    ld.add_action(get_robot_name("tiago"))
+    ld.add_action(OpaqueFunction(function=declare_args))
     ld.add_action(sim_time_arg)
-    ld.add_action(run_move_group_node)
+
+    # Execute move_group node
+    ld.add_action(OpaqueFunction(function=launch_setup))
 
     return ld
--- launch/moveit_rviz.launch.py
+++ launch/moveit_rviz.launch.py
@@ -1,36 +1,51 @@
 import os
-import yaml
+from pathlib import Path
+
 from launch import LaunchDescription
+from launch.actions import OpaqueFunction
 from launch_ros.actions import Node
+from launch_pal.arg_utils import read_launch_argument
+from launch_pal.robot_utils import (get_arm,
+                                    get_camera_model,
+                                    get_end_effector,
+                                    get_ft_sensor,
+                                    get_laser_model,
+                                    get_robot_name,
+                                    get_wrist_model)
 from launch_pal.substitutions import LoadFile
 from ament_index_python.packages import get_package_share_directory
 
-from tiago_description.tiago_launch_utils import (get_tiago_hw_arguments,
-                                                  get_tiago_hw_suffix,
-                                                  generate_robot_description_action)
+from tiago_description.tiago_launch_utils import get_tiago_hw_suffix
+from launch_param_builder import load_xacro, load_yaml
 
-def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
 
-    try:
-        with open(absolute_file_path, "r") as file:
-            return yaml.safe_load(file)
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
+def declare_args(context, *args, **kwargs):
 
-def generate_launch_description():
+    robot_name = read_launch_argument('robot_name', context)
+
     # TIAGo description arguments
-    tiago_args = get_tiago_hw_arguments(
-        laser_model=True,
-        arm=True,
-        end_effector=True,
-        ft_sensor=True,
-        camera_model=True)
+    return [get_arm(robot_name),
+            get_camera_model(robot_name),
+            get_end_effector(robot_name),
+            get_ft_sensor(robot_name),
+            get_laser_model(robot_name),
+            get_wrist_model(robot_name)]
 
-    # Read robot descripton from tiago_description
-    robot_description = {
-        "robot_description": generate_robot_description_action()}
+
+def launch_setup(context, *args, **kwargs):
+
+    robot_description = {'robot_description': load_xacro(
+        Path(os.path.join(
+            get_package_share_directory('tiago_description'), 'robots', 'tiago.urdf.xacro')),
+        {
+            'arm': read_launch_argument('arm', context),
+            'camera_model': read_launch_argument('camera_model', context),
+            'end_effector': read_launch_argument('end_effector', context),
+            'ft_sensor': read_launch_argument('ft_sensor', context),
+            'laser_model': read_launch_argument('laser_model', context),
+            'wrist_model': read_launch_argument('wrist_model', context),
+        },
+    )}
 
     robot_description_semantic_config = LoadFile(
         [get_package_share_directory("tiago_moveit_config"), "/config/srdf/tiago_",
@@ -42,7 +57,8 @@ def generate_launch_description():
     }
 
     kinematics_yaml = load_yaml(
-        "tiago_moveit_config", "config/kinematics_kdl.yaml"
+        Path(os.path.join(
+            get_package_share_directory('tiago_moveit_config'), 'config', 'kinematics_kdl.yaml'))
     )
 
     # Planning Functionality
@@ -54,7 +70,8 @@ def generate_launch_description():
         }
     }
     ompl_planning_yaml = load_yaml(
-        "tiago_moveit_config", "config/ompl_planning.yaml"
+        Path(os.path.join(
+            get_package_share_directory('tiago_moveit_config'), 'config', 'ompl_planning.yaml'))
     )
     ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
 
@@ -76,9 +93,19 @@ def generate_launch_description():
         ],
     )
 
+    return [rviz_node]
+
+
+def generate_launch_description():
+
     ld = LaunchDescription()
 
-    ld.add_action(*tiago_args)
-    ld.add_action(rviz_node)
+    # Declare arguments
+    # we use OpaqueFunction so the callbacks have access to the context
+    ld.add_action(get_robot_name("tiago"))
+    ld.add_action(OpaqueFunction(function=declare_args))
+
+    # Execute move_group node
+    ld.add_action(OpaqueFunction(function=launch_setup))
 
     return ld
