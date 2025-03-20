ac3907689a317ddc4fe8627968f96cb03874a7b9
matteovillani <matteo.villani@pal-robotics.com>
2025-03-20 16:11:28 +0100

    cheks if the string 'use_sensor_manager' is equal to true

--- launch/move_group.launch.py
+++ launch/move_group.launch.py
@@ -117,7 +117,7 @@ def start_move_group(context, *args, **kwargs):
         .pilz_cartesian_limits(file_path=pilz_cartesian_limits)
     )
 
-    if use_sensor_manager:
+    if use_sensor_manager == "True":
         # moveit_sensors path
         moveit_sensors_path = "config/sensors_3d.yaml"
         moveit_config.sensors_3d(moveit_sensors_path)
